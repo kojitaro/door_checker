8620c10f9c4052a9cde71ceece6b5957a0ee44b2
kojitaro <kohura12345678@gmail.com>
2017-12-02 18:27:06 +0900

    add files

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# coding: UTF-8
+
+from serial import *
+import sys
+import twelite
+
+
+def parse(line):
+    if line[0] != ":":
+        return
+    
+    line = line[1:]
+    data = twelite.parse_readdata(line)
+    print(data)
+
+
+def main(port):
+# シリアルポートを開く
+    try:
+        ser = Serial(port, 115200)
+        print "open serial port: %s" % port
+    except:
+        print "cannot open serial port: %s" % port
+        exit(1)
+
+    while True:
+        line = ser.readline().rstrip()
+        parse(line)
+
+
+if __name__ == '__main__':
+    port = sys.argv[1]
+    main(port)
---
+++ twelite.py
@@ -0,0 +1,25 @@
+import binascii
+import struct
+
+recvfmt = struct.Struct("<BBBBBIBHBHBBBBBBBBB")
+
+def parse_readdata(line):
+    data = recvfmt.unpack(binascii.a2b_hex(line))
+
+    return {
+        "sender_id":data[0],
+        "command_no":data[1],
+        "packet_identity":data[2],
+        "protocol_version":data[3],
+        "lqi":data[4],
+        "sender_number":data[5],
+        "receiver_id":data[6],
+        "timestamp":data[7],
+        "relay_flag":data[8],
+        "voltage":data[9],
+        "di":[data[11]&0x01, (data[11]&0x02)>>1, (data[11]&0x04)>>2, (data[11]&0x08)>>3],
+        "di_change_flag":data[12],
+        "ai":[data[13],data[14],data[15],data[16]],
+        "ai_efn":data[17],
+        "checksum":data[18],
+    }
